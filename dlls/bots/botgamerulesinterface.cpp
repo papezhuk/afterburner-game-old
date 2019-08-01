aa6caf62782c662e6e9e933cdee4c9512f88054a
Jonathan Poncelet <jonathan.poncelet@talk21.com>
2019-08-01 07:24:53 +0100

    Added simple add/remove bot commands

--- dlls/bots/botgamerulesinterface.cpp
+++ dlls/bots/botgamerulesinterface.cpp
@@ -13,25 +13,37 @@
 
 CBotGameRulesInterface::CBotGameRulesInterface(CGameRules* parent) :
 	m_pParent(parent),
-	m_nNextBotNumber(0)
+	m_nNextBotNumber(0),
+	m_nBotCount(0)
 {
 	ASSERT(m_pParent);
 }
 
-void CBotGameRulesInterface::ClientDisconnect(edict_t* pEntity)
+void CBotGameRulesInterface::ClientDisconnect(edict_t* entity)
 {
-}
-
-void CBotGameRulesInterface::ClientPutInServer(edict_t* pEntity)
-{
-	if ( !pEntity || !(pEntity->v.flags & FL_FAKECLIENT) )
+	if ( !entity || !(entity->v.flags & FL_FAKECLIENT) )
 	{
 		return;
 	}
 
+	if ( m_nBotCount > 0 )
+	{
+		--m_nBotCount;
+	}
+}
+
+void CBotGameRulesInterface::ClientPutInServer(edict_t* entity)
+{
+	if ( !entity || !(entity->v.flags & FL_FAKECLIENT) )
+	{
+		return;
+	}
+
+	++m_nBotCount;
+
 	// This step converts the entity into a bot
 	// by linking up the edict to the CBaseBot class.
-	CBaseBot* bot = GetClassPtrFromEdict<CBaseBot>(pEntity);
+	CBaseBot* bot = GetClassPtrFromEdict<CBaseBot>(entity);
 
 	// Should never happen, but to be safe:
 	if ( !bot )
@@ -42,13 +54,17 @@ void CBotGameRulesInterface::ClientPutInServer(edict_t* pEntity)
 	m_pParent->PlayerThink(bot);
 }
 
-bool CBotGameRulesInterface::ClientCommand(CBasePlayer* pPlayer, const char* pcmd)
+bool CBotGameRulesInterface::ClientCommand(CBasePlayer* player, const char* command)
 {
 	bool handled = true;
 
-	if ( FStrEq(pcmd, "bot_add") )
+	if ( FStrEq(command, "bot_add") )
 	{
-		CreateBot();
+		HandleBotAddCommand(player);
+	}
+	else if ( FStrEq(command, "bot_remove_all") )
+	{
+		HandleBotRemoveAllCommand(player);
 	}
 	else
 	{
@@ -80,26 +96,113 @@ void CBotGameRulesInterface::Think()
 	}
 }
 
-void CBotGameRulesInterface::CreateBot()
+bool CBotGameRulesInterface::CanExecuteCommand(CBasePlayer* player)
 {
-	std::string name = std::string("Bot") + std::to_string(m_nNextBotNumber++);
-	edict_t* bot = g_engfuncs.pfnCreateFakeClient(name.c_str());
-
-	if ( !bot )
+	if ( IS_DEDICATED_SERVER() )
 	{
-		static const char* errorMsg = "There are no free client slots. Set maxplayers to a higher value.\n";
+		// Client commands not allowed to be processed on a dedicated server.
+		CLIENT_PRINTF(player->edict(), print_console, "Bots can only be added or removed by the server.\n");
+		return false;
+	}
 
-		UTIL_ClientPrintAll(HUD_PRINTNOTIFY, UTIL_VarArgs(errorMsg));
+	return true;
+}
 
-		if (IS_DEDICATED_SERVER())
+void CBotGameRulesInterface::HandleBotAddCommand(CBasePlayer* player)
+{
+	if ( !CanExecuteCommand(player) )
+	{
+		return;
+	}
+
+	int numArgs = CMD_ARGC();
+
+	if ( numArgs < 2 )
+	{
+		// Add a new bot with a random profile.
+		CreateBots(1);
+		return;
+	}
+
+	if ( numArgs == 2 )
+	{
+		const char* botCountStr = CMD_ARGV(1);
+		int botCount = atoi(botCountStr);
+
+		if ( botCount > 0 )
 		{
-			printf(errorMsg);
+			// Add a specified number of bots.
+			CreateBots(botCount);
+		}
+		else if ( botCount == 0 )
+		{
+			// See whether the argument matches a bot profile.
+			// TODO
 		}
 
 		return;
 	}
 
-	// Run the bot through the standard connection functions.
-	// It'll eventually get passed back through this class.
-	::ClientPutInServer(bot);
+	for ( int argNum = 1; argNum < numArgs; ++argNum )
+	{
+		// Treat each argument as a bot profile.
+		const char* arg = CMD_ARGV(argNum);
+
+		// TODO
+	}
+}
+
+void CBotGameRulesInterface::HandleBotRemoveAllCommand(CBasePlayer* player)
+{
+	if ( !CanExecuteCommand(player) )
+	{
+		return;
+	}
+
+	std::string kickCommands;
+
+	for ( int clientIndex = 1; clientIndex <= gpGlobals->maxClients; ++clientIndex )
+	{
+		CBasePlayer* player = UTIL_CBasePlayerByIndex(clientIndex);
+
+		if ( !player || !player->IsBot() )
+		{
+			continue;
+		}
+
+		const char* name = STRING(player->pev->netname);
+		kickCommands += std::string("kick \"") + std::string(name) + std::string("\"\n");
+	}
+
+	if ( kickCommands.size() > 0 )
+	{
+		SERVER_COMMAND(kickCommands.c_str());
+	}
+}
+
+void CBotGameRulesInterface::CreateBots(uint32_t num)
+{
+	for ( uint32_t iteration = 0; iteration < num; ++iteration )
+	{
+		std::string name = std::string("Bot") + std::to_string(m_nNextBotNumber++);
+		edict_t* bot = g_engfuncs.pfnCreateFakeClient(name.c_str());
+
+		if ( !bot )
+		{
+			static const char* errorMsg = "No free player slots to create new bot.\n";
+
+			UTIL_ClientPrintAll(HUD_PRINTNOTIFY, UTIL_VarArgs(errorMsg));
+
+			if ( IS_DEDICATED_SERVER() )
+			{
+				printf(errorMsg);
+			}
+
+			return;
+		}
+
+		// Run the bot through the standard connection functions.
+		// It'll eventually get passed back through this class.
+		::ClientPutInServer(bot);
+	}
 }
--- dlls/bots/botgamerulesinterface.h
+++ dlls/bots/botgamerulesinterface.h
@@ -13,16 +13,21 @@ class CBotGameRulesInterface
 public:
 	CBotGameRulesInterface(CGameRules* parent);
 
-	void ClientDisconnect(edict_t* pEntity);
-	void ClientPutInServer(edict_t* pEntity);
-	bool ClientCommand(CBasePlayer* pPlayer, const char* pcmd);
+	void ClientDisconnect(edict_t* entity);
+	void ClientPutInServer(edict_t* entity);
+	bool ClientCommand(CBasePlayer* player, const char* command);
 	void Think();
 
-	void CreateBot();
+	void CreateBots(uint32_t num);
 
 private:
+	bool CanExecuteCommand(CBasePlayer* player);
+	void HandleBotAddCommand(CBasePlayer* player);
+	void HandleBotRemoveAllCommand(CBasePlayer* player);
+
 	CGameRules* m_pParent;
 	uint32_t m_nNextBotNumber;
+	uint32_t m_nBotCount;
 };
 
 #endif // BOTGAMERULESINTERFACE_H
